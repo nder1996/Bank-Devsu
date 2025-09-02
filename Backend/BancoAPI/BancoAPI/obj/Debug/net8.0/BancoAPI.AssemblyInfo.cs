49ea35966e09fd85f84c57076514edfe6aca003a
Ander_AAM <72629491+nder1996@users.noreply.github.com>
2025-09-02 09:48:16 -0500

    solucion conflicto rutas docker

--- Backend/BancoAPI/BancoAPI/obj/Debug/net8.0/BancoAPI.AssemblyInfo.cs
+++ Backend/BancoAPI/BancoAPI/obj/Debug/net8.0/BancoAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BancoAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95442c3022f8186cdbaa2aa4afaf54face7474d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9b72393b657c151e923919aa421ca639f16d5b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BancoAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BancoAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
